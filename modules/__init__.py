f39aedc19859a1b7d1366b5fc28d2a7d958f6d9c
deprekate <deprekate@gmail.com>
2020-05-05 22:25:30 -0700

    forgot the init

---
+++ modules/__init__.py
@@ -0,0 +1,15 @@
+
+from .file_handling import read_fasta
+from .edges import Edge
+from .nodes import Node
+from .orfs import Orf
+
+
+__all__ = [
+			'file_handling',
+			'functions',
+			'edges',
+			'nodes',
+			'orfs',
+			'gc_frame_plot'
+			]

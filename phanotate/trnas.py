eb9899c68348ccc1e2975e985926b7df38e3922b
deprekate <deprekate@gmail.com>
2021-02-19 15:48:29 -0800

    Reduced the tRNA weight from -20 to -1 for testing

--- phanotate/trnas.py
+++ phanotate/trnas.py
@@ -36,7 +36,7 @@ class tRNA:
 		self.start  = int(start)
 		self.stop    = int(stop)
 		self.type   = type_
-		self.weight = Decimal(-20)
+		self.weight = Decimal(-1)
 		self.frame  = 4 if start < stop else -4
 
 	def as_edge(self):

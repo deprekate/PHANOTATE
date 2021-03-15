3eee2ea97785cb260c72ca13bba2ee9ebed46bd1
deprekate <deprekate@gmail.com>
2021-03-15 15:59:10 -0700

    Switched trna weight back to -20

--- phanotate.py
+++ phanotate.py
@@ -23,23 +23,30 @@ args = file_handling.get_args()
 #                               FILE INPUT                                                         #
 #--------------------------------------------------------------------------------------------------#
 
+import time
+start = time.time()
 base_trans = str.maketrans('SBVDEFHIJKLMNOPQRUWXYZ','GGGAAAAAAAAAAAAAAAAAAA')
 contigs = file_handling.read_fasta(args.infile, base_trans)
 if not contigs:
 	sys.stdout.write("Error: no sequences found in infile\n")
 	sys.exit()
 
+print("Input", time.time() - start)
 
 #--------------------------------------------------------------------------------------------------#
 #                               MAIN ROUTINE                                                       #
 #--------------------------------------------------------------------------------------------------#
 for id, dna in contigs.items():
+	start = time.time()
 	contig_features = Features(**vars(args))
 	#-------------------------------Find the ORFs----------------------------------------------#
 
 	contig_features.parse_contig(id, dna)
+	print("Input", time.time() - start)
 
+	start = time.time()
 	contig_features.score_orfs()
+	print("Score", time.time() - start)
 
 
 	# find other features
@@ -58,6 +65,7 @@ for id, dna in contigs.items():
 	for orf in contig_features:
 		ret = fz.add_edge( orf.as_edge() )
 
+	start = time.time()
 	# write edges to pconnect to get interconnections
 	for edge in fz.get_edges():
 		ret = pc.add_edge( edge )
@@ -70,6 +78,7 @@ for id, dna in contigs.items():
 	# write connections to the graph
 	for edge in pc.get_connections(pnots=contig_features.pnots):
 		ret = fz.add_edge( edge )
+	print("Connect", time.time() - start)
 
 	if args.dump:
 		for edge in fz.get_edges():
@@ -81,10 +90,14 @@ for id, dna in contigs.items():
 		sys.exit()
 	#-------------------------------Run Bellman-Ford-------------------------------------------#
 
+	start = time.time()
 	shortest_path = fz.get_path(source= contig_features.source_node(), target= contig_features.target_node())[1:-1]
+	print("Bellman", time.time() - start)
 
 	#-------------------------------Write Output ----------------------------------------------#
+	start = time.time()
 	file_handling.write_output(contig_features, shortest_path)
+	print("Output", time.time() - start)
 
 #--------------------------------------------------------------------------------------------------#
 #                               END                                                                #
